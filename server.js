3f5bacf77aa0e130dc960b263df91576f80c1028
lulu kuma <brookehdiaz@gmail.com>
2024-05-15 15:55:55 +0000

    question mark

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 app.use(session({
-  secret: SESSION_SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
