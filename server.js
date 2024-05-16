e80a0d0ef895aa2ab7d4641d5e49a0d006a2a8ad
lulu kuma <brookehdiaz@gmail.com>
2024-05-16 14:46:34 +0000

    seems wrong

--- server.js
+++ server.js
@@ -28,19 +28,42 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
 
-passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
- // });
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, null);
+   });
+  });
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
+// app.listen out here...
+
+//app.route('/').get((req, res) => {
+//res.render('index', { title: 'Hello', message: 'Please log in' });
+//});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
