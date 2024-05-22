95199a3c451c80508b4dd90639933cb89dcdada2
lulu kuma <brookehdiaz@gmail.com>
2024-05-22 15:29:45 +0000

    forgot success and fail

--- server.js
+++ server.js
@@ -72,7 +72,17 @@ myDB(async client => {
   });
 });
 // app.listen out here...
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
