26ecee13fe41223e772131a4e0bbcd4daad89593
lulu kuma <brookehdiaz@gmail.com>
2024-05-22 14:45:41 +0000

    added oauth

--- routes.js
+++ routes.js
@@ -8,7 +8,8 @@ module.exports = function (app, myDataBase) {
           title: 'Connected to Database',
           message: 'Please login',
           showLogin: true,
-          showRegistration: true
+          showRegistration: true,
+          showSocialAuth: true
         });
       });
     
@@ -57,6 +58,12 @@ module.exports = function (app, myDataBase) {
         req.logout();
         res.redirect('/');
     });
+
+    app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+
     app.use((req, res, next) => {
       res.status(404)
         .type('text')
