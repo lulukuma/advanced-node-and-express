cece22524541048a7e4cec19a4c006fa9cfb89e5
lulu kuma <brookehdiaz@gmail.com>
2024-05-22 15:54:43 +0000

    added more stuff

--- public/client.js
+++ public/client.js
@@ -8,6 +8,10 @@ socket.on('user', data => {
     (data.connected ? ' has joined the chat.' : ' has left the chat.');
   $('#messages').append($('<li>').html('<b>' + message + '</b>'));
 });
+socket.on('chat message', data => {
+  console.log('socket.on 1')
+  $('#messages').append($('<li>').text(`${data.username}: ${data.message}`));
+})
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     let messageToSend = $('#m').val();
